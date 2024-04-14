fbd11e9b66a7f560f698ceafffabddb468bcc2a2
Mohd Ghouse Tazeem <ghousetazeem75@gmail.com>
2024-04-14 13:19:03 +0530

    Final Commit

--- context-toggle/vite.config.js
+++ context-toggle/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
+import tailwindcss from 'tailwindcss'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['react-router-dom']
+    }
+  },
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  }
 })
